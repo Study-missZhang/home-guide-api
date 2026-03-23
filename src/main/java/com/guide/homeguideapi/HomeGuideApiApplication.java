ffddade09326f630e55c42b3ad90c3317246f26b
Study-missZhang <1036254540@qq.com>
2026-03-23 16:51:35 +0800

    - 项目初始化

---
+++ src/main/java/com/guide/homeguideapi/HomeGuideApiApplication.java
@@ -0,0 +1,13 @@
+package com.guide.homeguideapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeGuideApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeGuideApiApplication.class, args);
+    }
+
+}
